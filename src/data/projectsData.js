617aa34210bac91b1d17f13dfc39a7c3e04aa9dc
Shubham Dubey <shubhamdubey.dubey4@gmail.com>
2022-08-18 20:55:04 +0530

    teerex project added

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,92 +1,99 @@
-import ADMIN_UI from "../assets/png/ADMIN_UI.png"
-import MERN_APP from "../assets/png/MERN_APP.png"
-import Sportsjam from '../assets/png/Sportsjam.png'
-import Tanishq from '../assets/png/Tanishq.png'
-import eight from '../assets/svg/projects/eight.svg'
-import five from '../assets/svg/projects/five.svg'
-import four from '../assets/svg/projects/four.svg'
-import myntra from '../assets/png/myntra.png'
-import one from '../assets/svg/projects/one.svg'
-import seven from '../assets/svg/projects/seven.svg'
-import six from '../assets/svg/projects/six.svg'
-import three from '../assets/svg/projects/three.svg'
-import two from '../assets/svg/projects/two.svg'
+import ADMIN_UI from '../assets/png/ADMIN_UI.png';
+import MERN_APP from '../assets/png/MERN_APP.png';
+import Sportsjam from '../assets/png/Sportsjam.png';
+import Tanishq from '../assets/png/Tanishq.png';
+import TeerexStore from '../assets/png/TeerexStore.png';
+import myntra from '../assets/png/myntra.png';
 
 export const projectsData = [
-    {
-        id: 1,
-        projectName: 'Myntra.com',
-        projectDesc: 'Myntra website Clone Features:Login_SignUp||Landing Page||Product Section||Payment & CheckOut',
-        tags: ['HTML', 'CSS','Javascript','FrontEnd',],
-        code: 'https://github.com/Luke0912/myntra-app',
-        demo: 'https://quizzical-goldstine-ff92bf.netlify.app/',
-        image: myntra
-    },
-    {
-        id: 2,
-        projectName: 'Tanishq.com',
-        projectDesc: 'Tanishq website Clone with real time backend database management Features:Login_SignUp||Landing Page||Product Section||Payment & CheckOut',
-        tags: ['HTML', 'CSS','Javascript','NodeJS','FrontEnd','BackEnd'],
-        code: 'https://github.com/Luke0912/Tanishq',
-        demo: 'https://heuristic-minsky-e408f0.netlify.app/',
-        image: Tanishq
-    },
-    {
-        id: 3,
-        projectName: 'Sports-Jam',
-        projectDesc: 'Sports-Jam(E-Commerce sports website) website Clone backend database management Features:Login_SignUp||Landing Page||Product Section||Payment & CheckOut.  React||Node.js||Redux||Mongo||Json-server',
-        tags: ["React",'NodeJS','FrontEnd','BackEnd'],
-        code: 'https://github.com/Luke0912/Sportsjam',
-        demo: 'https://sports-jam.netlify.app/',
-        image: Sportsjam
-    },
-    {
-        id: 4,
-        projectName: 'Admin-UI',
-        projectDesc: "TABLE || UI || PAGINATION || INMEMORY-EDIT & DELETE ||ROW-SELECTION",
-        tags: ["React","FrontEnd"],
-        code: 'https://github.com/Luke0912/Admin-UI',
-        demo: 'https://admin-ui-geektr.netlify.app/',
-        image: ADMIN_UI
-    },
-    {
-        id: 5,
-        projectName: 'Mern-App',
-        projectDesc: 'Mern Project with features:- Users should be able to sign up and login with email and password. After login, he can add or edit recipes of dishes. Other users can login and see dishes posted by all users. Tech-Stack Used React,Express,MongoDB,Node.Js',
-        tags: ["FrontEnd","BackEnd","React","MongoDB","Express","NodeJS"],
-        code: 'https://github.com/Luke0912/Mern-Practical-Assesment',
-        demo: 'https://mern-practical-assesment.netlify.app/',
-        image: MERN_APP
-    },
-    // {
-    //     id: 6,
-    //     projectName: 'Uber Lite',
-    //     projectDesc: 'Uber clone',
-    //     tags: ['Flutter'],
-    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     image: six
-    // },
-    // {
-    //     id: 7,
-    //     projectName: 'Stock Market App',
-    //     projectDesc: 'A simple stock market API app',
-    //     tags: ['React', 'Redux', 'Bootstrap'],
-    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     image: seven
-    // },
-    // {
-    //     id: 8,
-    //     projectName: 'Car Pooling System',
-    //     projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-    //     tags: ['Flutter', 'React'],
-    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     image: eight
-    // },
-]
-
+  {
+    id: 1,
+    projectName: 'Myntra.com',
+    projectDesc:
+      'Myntra website Clone Features:Login_SignUp||Landing Page||Product Section||Payment & CheckOut',
+    tags: ['HTML', 'CSS', 'Javascript', 'FrontEnd'],
+    code: 'https://github.com/Luke0912/myntra-app',
+    demo: 'https://quizzical-goldstine-ff92bf.netlify.app/',
+    image: myntra,
+  },
+  {
+    id: 2,
+    projectName: 'Tanishq.com',
+    projectDesc:
+      'Tanishq website Clone with real time backend database management Features:Login_SignUp||Landing Page||Product Section||Payment & CheckOut',
+    tags: ['HTML', 'CSS', 'Javascript', 'NodeJS', 'FrontEnd', 'BackEnd'],
+    code: 'https://github.com/Luke0912/Tanishq',
+    demo: 'https://heuristic-minsky-e408f0.netlify.app/',
+    image: Tanishq,
+  },
+  {
+    id: 3,
+    projectName: 'Sports-Jam',
+    projectDesc:
+      'Sports-Jam(E-Commerce sports website) website Clone backend database management Features:Login_SignUp||Landing Page||Product Section||Payment & CheckOut.  React||Node.js||Redux||Mongo||Json-server',
+    tags: ['React', 'NodeJS', 'FrontEnd', 'BackEnd'],
+    code: 'https://github.com/Luke0912/Sportsjam',
+    demo: 'https://sports-jam.netlify.app/',
+    image: Sportsjam,
+  },
+  {
+    id: 4,
+    projectName: 'Admin-UI',
+    projectDesc:
+      'TABLE || UI || PAGINATION || INMEMORY-EDIT & DELETE ||ROW-SELECTION',
+    tags: ['React', 'FrontEnd'],
+    code: 'https://github.com/Luke0912/Admin-UI',
+    demo: 'https://admin-ui-geektr.netlify.app/',
+    image: ADMIN_UI,
+  },
+  {
+    id: 5,
+    projectName: 'Mern-App',
+    projectDesc:
+      'Mern Project with features:- Users should be able to sign up and login with email and password. After login, he can add or edit recipes of dishes. Other users can login and see dishes posted by all users. Tech-Stack Used React,Express,MongoDB,Node.Js',
+    tags: ['FrontEnd', 'BackEnd', 'React', 'MongoDB', 'Express', 'NodeJS'],
+    code: 'https://github.com/Luke0912/Mern-Practical-Assesment',
+    demo: 'https://mern-practical-assesment.netlify.app/',
+    image: MERN_APP,
+  },
+  {
+    id: 6,
+    projectName: 'React-Redux Store',
+    projectDesc:
+      'Search Bar || Product Page || Category wise Filter || Add to cart || Edit cart item',
+    tags: ['FrontEnd', 'React', 'Redux', 'JavaScript'],
+    code: 'https://github.com/Luke0912/TeeRex_store',
+    demo: 'https://teerex-shopee.netlify.app/',
+    image: TeerexStore,
+  },
+  // {
+  //     id: 6,
+  //     projectName: 'Uber Lite',
+  //     projectDesc: 'Uber clone',
+  //     tags: ['Flutter'],
+  //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+  //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+  //     image: six
+  // },
+  // {
+  //     id: 7,
+  //     projectName: 'Stock Market App',
+  //     projectDesc: 'A simple stock market API app',
+  //     tags: ['React', 'Redux', 'Bootstrap'],
+  //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+  //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+  //     image: seven
+  // },
+  // {
+  //     id: 8,
+  //     projectName: 'Car Pooling System',
+  //     projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
+  //     tags: ['Flutter', 'React'],
+  //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+  //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+  //     image: eight
+  // },
+];
 
 // Do not remove any fields.
 // Leave it blank instead as shown below
